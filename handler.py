60de0a008d4bdb72a19b85de09125e147d57ed24
Rafael Oening <rafael.oening@rentcars.com>
2018-09-10 16:08:12 -0300

    teste

--- handler.py
+++ handler.py
@@ -1,2 +1,15 @@
-def handler(event, context):
-	print('teste')
+from Chalice import Chalice, Response, CORSConfig
+import os
+import json
+
+cors_config = CORSConfig(
+    allow_origin='*',
+    allow_headers=['Content-Type','X-Amz-Date','Authorization','X-Api-Key','X-Amz-Security-Token'],
+    allow_credentials=True
+)
+
+app = Chalice(app_name='lambda-python-sample')
+
+@app.route('/', methods=['GET'], cors=cors_config)
+def rootFunction():
+    Response(body={'Status': 'Ok'}, status_code=200, headers={'Content-Type': 'application/json'})
